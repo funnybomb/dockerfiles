b4975eb26bff6dfee29c0c03b4b1c2d0189249c1
fcy <fcy@mgt.(none)>
2018-08-14 14:38:41 +0800

    ZZ

--- dockerfile.R
+++ dockerfile.R
@@ -1,4 +1,4 @@
-# Copyright (c) Jupyter Development Team.
+# build R image in Jupyter
 # Distributed under the terms of the Modified BSD License.
 
 # Ubuntu 18.04 (bionic) from 2018-04-26
@@ -18,4 +18,5 @@ RUN $CONDA_DIR/bin/conda install --quiet --yes r r-essentials && \
     $CONDA_DIR/bin/conda clean -tipsy && \
     npm cache clean --force && \
     rm -rf $CONDA_DIR/share/jupyter/lab/staging && \
-    rm -rf /home/$NB_USER/.cache/yarn
+    rm -rf /home/$NB_USER/.cache/yarn && \
+    fix-permissions /home/$NB_USER
